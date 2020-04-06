bab4c80d55fc44a297f1560dd1d9818e1733d601
santiago <santiagocba@hotmail.com>
2020-04-06 18:11:23 -0300

    refetchQueries not need

--- final/client/src/containers/book-trips.js
+++ final/client/src/containers/book-trips.js
@@ -3,9 +3,7 @@ import { useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
 import Button from '../components/button';
-import { GET_LAUNCH } from './cart-item';
 
-export { GET_LAUNCH };
 export const BOOK_TRIPS = gql`
   mutation BookTrips($launchIds: [ID]!) {
     bookTrips(launchIds: $launchIds) {
@@ -24,10 +22,6 @@ export default function BookTrips({ cartItems }) {
     BOOK_TRIPS,
     {
       variables: { launchIds: cartItems },
-      /*refetchQueries: cartItems.map(launchId => ({
-        query: GET_LAUNCH,
-        variables: { launchId },
-      })),*/
       update(cache) {
         cache.writeData({ data: { cartItems: [] } });
       }
